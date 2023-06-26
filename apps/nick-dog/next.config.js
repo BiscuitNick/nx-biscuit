4adce3ffbd3e188823ff1a97007cee81b616a4c0
Biscuit Nick <nick@biscuit.land>
2023-06-26 15:22:06 -0700

    0.0.8 | troubleshooting build issue -- ZLIB_1.2.9 not found

--- apps/nick-dog/next.config.js
+++ apps/nick-dog/next.config.js
@@ -14,6 +14,17 @@ const nextConfig = {
     svgr: false,
   },
   webpack: (config) => {
+    if (
+      process.env.LD_LIBRARY_PATH == null ||
+      !process.env.LD_LIBRARY_PATH.includes(
+        `${process.env.PWD}/node_modules/canvas/build/Release:`
+      )
+    ) {
+      process.env.LD_LIBRARY_PATH = `${
+        process.env.PWD
+      }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+    }
+
     config.externals.push({
       sharp: 'commonjs sharp',
       canvas: 'commonjs canvas',
@@ -21,17 +32,6 @@ const nextConfig = {
     return config;
   },
 };
-
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
