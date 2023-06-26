0195010a3659e03b0a9f65635a09d562f037747b
Biscuit Nick <nick@biscuit.land>
2023-06-26 16:01:19 -0700

    0.0.9 - more build errors

--- apps/nick-dog/next.config.js
+++ apps/nick-dog/next.config.js
@@ -31,6 +31,10 @@ const nextConfig = {
     });
     return config;
   },
+  experimental: {
+    legacyBrowsers: false,
+    outputFileTracingIgnores: ['**canvas**'],
+  },
 };
 
 const plugins = [
