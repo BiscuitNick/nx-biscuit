b346b73b1083b7e379bc34710f437397a81f9e04
Biscuit Nick <nick@biscuit.land>
2023-06-26 15:16:41 -0700

    0.0.8

--- apps/nick-dog/next.config.js
+++ apps/nick-dog/next.config.js
@@ -6,6 +6,7 @@ const { composePlugins, withNx } = require('@nx/next');
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
+
 const nextConfig = {
   nx: {
     // Set this to true if you would like to use SVGR
@@ -20,6 +21,17 @@ const nextConfig = {
     return config;
   },
 };
+
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
