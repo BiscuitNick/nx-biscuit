94f70889eb92c03fb5a3e1b0e32c5cda76a52d16
Biscuit Nick <nick@biscuit.land>
2023-08-01 17:31:51 -0700

    0.1.8-b

--- apps/nick-dog/pages/component-blog/[slug].tsx
+++ apps/nick-dog/pages/component-blog/[slug].tsx
@@ -46,7 +46,7 @@ export default function Page({ content }: ContentArray) {
         return c.type === 'text' ? (
           <div key={i}>{c.content}</div>
         ) : c.type === 'video-poker' ? (
-          <VideoPoker key={i} />
+          <div>{c.type}</div>
         ) : null;
       })}
     </main>
