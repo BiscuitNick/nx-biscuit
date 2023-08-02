67e3c5d8866ea01e370202a8b41e35a10503f8ac
Biscuit Nick <nick@biscuit.land>
2023-08-01 17:02:42 -0700

    0.1.8

---
+++ apps/nick-dog/pages/component-blog/[slug].tsx
@@ -0,0 +1,54 @@
+import { VideoPoker } from '@nx-biscuit/biscuit-cards';
+
+const STRAPI_API_PATH = process.env.STRAPI_API_PATH;
+
+export function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true, // false or "blocking"
+  };
+}
+
+export async function getStaticProps({
+  params: { slug },
+}: {
+  params: { slug: string };
+}) {
+  const res = await fetch(
+    `${STRAPI_API_PATH}/api/component-posts?filters[slug][$eq]=${slug}`
+  );
+
+  const { data } = await res.json();
+  const {
+    attributes: { content },
+  } = data[0];
+
+  return {
+    props: {
+      content,
+    },
+  };
+}
+
+interface ContentItem {
+  type: string;
+  content?: string;
+}
+
+interface ContentArray {
+  content: ContentItem[];
+}
+
+export default function Page({ content }: ContentArray) {
+  return (
+    <main className="text-white">
+      {content?.map((c: ContentItem, i: number) => {
+        return c.type === 'text' ? (
+          <div key={i}>{c.content}</div>
+        ) : c.type === 'video-poker' ? (
+          <VideoPoker key={i} />
+        ) : null;
+      })}
+    </main>
+  );
+}
